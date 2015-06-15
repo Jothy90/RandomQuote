63296e106aac298d0411016376f50c3bde353a7c
Jothy90 <ajohnnirajh@gmail.com>
2015-06-15 14:02:40 +0530

    Initial

---
+++ src/com/kash/rmi/Quote.java
@@ -0,0 +1,10 @@
+package com.kash.rmi; /**
+ * Created by john on 6/15/15.
+ */
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Quote extends Remote {
+    String getQuoted() throws RemoteException;
+}
+
